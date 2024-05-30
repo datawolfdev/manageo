7038a7965d9dcecbc1d3ebf75640ddc202de7018
datawolfdev <106931831+Iceprod8@users.noreply.github.com>
2024-05-30 14:49:51 +0200

    Update

--- pages/api/email/enrich.js
+++ pages/api/email/enrich.js
@@ -1,4 +1,5 @@
 import { enrich } from "@/api_management/function";
+import { pool } from "@/db";
 
 export default async function handler(req, res) {
     const { nSiret, compagnies, Contact } = req.body;
@@ -6,7 +7,7 @@ export default async function handler(req, res) {
         const { success, message } = await enrich(compagnies.filter(Boolean), Contact)
         if (success) {
             const companyCount = compagnies.filter(name => name).length;
-            await pool.query("INSERT INTO operations (siret_count, company_count) VALUES ($1)", [nSiret, companyCount]);
+            await pool.query("INSERT INTO operations (siret_count, company_count) VALUES ($1, $2)", [nSiret, companyCount]);
             return res.status(200).json({ success: true, message });
         } else {
             return res.status(500).json({ success: false, message });
